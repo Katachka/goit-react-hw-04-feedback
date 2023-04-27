e7bdd51e12dd45a33b93e74f9339bcabdb6cf581
Kateryna Ponomarenko <katachka.p@gmail.com>
2023-04-28 00:24:12 +0300

    hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,73 @@
+import { useState } from 'react';
+import { Section } from './Section/Section';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
+import { Notification } from './Notification/Notification';
+import { Container, Wrapper } from './App.styled';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+const handleClickButton = e => {
+  const option = e.target.name;
+  
+  switch (option) {
+    case 'good':
+      setGood(prevState => prevState + 1); 
+      break;
+     case 'neutral':
+      setNeutral(prevState => prevState + 1); 
+      break;
+     case 'bad':
+      setBad(prevState => prevState + 1); 
+       break;
+  
+    default:
+       console.log(`No option called ${option}`);
+      break;
+  }
+};  
+
+  const countTotalFeedback = () => good + neutral + bad;
+  
+const countPositiveFeedbackPercentage = () => {
+  const totalFeedback = countTotalFeedback();
+  const goodFeedback = good;
+  let result = 0;
+
+  if (totalFeedback > 0) {
+    result = Math.ceil((goodFeedback / totalFeedback) * 100);
+  }
+  return `${result}%`;
 };
+
+
+  return ( 
+    <Container>
+      <Wrapper>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={handleClickButton}
+          />
+        </Section>
+
+         <Section title="Statistics">
+            {countTotalFeedback > 0 ? (
+              <Statistics
+                good={good}
+                neutral={neutral}
+                bad={bad}
+                total={countTotalFeedback()}
+                positivePercentage={countPositiveFeedbackPercentage()}
+              />
+            ) : (
+              <Notification message="There is no feedback" />
+            )}
+          </Section>
+        </Wrapper>
+    </Container>
+  );
+}
---
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -0,0 +1,37 @@
+import styled from '@emotion/styled';
+
+export const List = styled.ul`
+display: flex;
+gap: 30px;
+`;
+export const Item = styled.li`
+ margin-bottom: 30px;
+
+ &:last-child {
+  margin-bottom: 0;
+}
+`;
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  font-family: inherit;
+  font-size: 16px;
+
+  width: 150px;
+  height: 40px;
+
+  background-color: var(--accent-bg-color);
+  color: var(--second-text-color);
+  border: none;
+  border-radius: 5px;
+
+  cursor: pointer;
+  transition: background-color var(--main-animation-hover);
+
+  &:hover,
+  &:focus {
+    background-color: var(--second-bg-color);
+  }  
+`;
